29042f33e1ecc5336406ed7a8ed8a76a8602a807
mascarenhas <mascarenhas>
2008-01-18 19:33:08 +0000

    *** empty log message ***

--- src/launcher/make_rc.lua
+++ src/launcher/make_rc.lua
@@ -2,7 +2,9 @@
 io.write("#define IDS_WSAPI 1\r\n")
 io.write("STRINGTABLE\r\nBEGIN\r\n")
 io.write("IDS_WSAPI \"")
-io.write("package.path = \"\"" .. package.path .. "\"\"\\n\\\r\n")
+io.write("package.path = [[" .. 
+    	package.path:gsub("\", "\\"):gsub('"', '""')
+	.. "]]\\n\\\r\n")
 
 for line in io.lines((...)) do
   if not line:match("^#!") then
