11e05417ceda6eb8557e50bd47849d1cae277f6c
mascarenhas <mascarenhas>
2008-01-21 00:44:35 +0000

    *** empty log message ***

--- src/launcher/make_rc.lua
+++ src/launcher/make_rc.lua
@@ -2,9 +2,6 @@
 io.write("#define IDS_WSAPI 1\r\n")
 io.write("STRINGTABLE\r\nBEGIN\r\n")
 io.write("IDS_WSAPI \"")
---[[io.write("package.path = [[?.lua;?/init.lua;" .. 
-    	package.path:gsub("\\", "\\\\"):gsub('"', '""')
-      .. "]]\\n\\\r\n")]]
 
 for line in io.lines((...)) do
   if not line:match("^#!") then
