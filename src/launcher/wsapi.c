4fd8d60535990cd8b5678491fc12f836aa4f5c31
mascarenhas <mascarenhas>
2008-01-23 18:29:15 +0000

    wsapi.c-based launchers now take an optional bootstrap

--- src/launcher/wsapi.c
+++ src/launcher/wsapi.c
@@ -6,7 +6,7 @@
 ** the Lua file in a protected environment just to redirect the error
 ** messages to stdout and stderr.
 **
-** $Id: wsapi.c,v 1.1 2008/01/18 19:25:21 mascarenhas Exp $
+** $Id: wsapi.c,v 1.2 2008/01/23 18:29:15 mascarenhas Exp $
 */
 
 #include <string.h>
@@ -40,8 +40,27 @@ static int runlua (lua_State *L, LPTSTR lua_string) {
         lua_gettable(L, -2);
 	err_func = lua_gettop (L);
 	return (luaL_loadstring (L, lua_string)) ||
-	  (lua_pcall (L, 0, 0, err_func));
+	  (lua_pcall (L, 0, LUA_MULTRET, err_func));
 }
+
+static DWORD GetModulePath( HINSTANCE hInst, LPTSTR pszBuffer, DWORD dwSize )
+//
+//	Return the size of the path in bytes.
+{
+	DWORD dwLength = GetModuleFileName( hInst, pszBuffer, dwSize );
+	if( dwLength )
+	{
+		while( dwLength && pszBuffer[ dwLength ] != '.' )
+		{
+			dwLength--;
+		}
+
+		if( dwLength )
+			pszBuffer[ dwLength ] = '\000';
+	}
+	return dwLength;
+}
+
 
 /*
 ** MAIN
@@ -53,17 +72,26 @@ int main (int argc, char *argv[]) {
 	TCHAR lua_string[10000];
 	lua_State *L = lua_open();
 	(void)argc; /* avoid "unused parameter" warning */
+	luaL_openlibs(L);
+	dwLength = GetModulePath( NULL, name, MAX_PATH );
+	if(dwLength) { /* Optional bootstrap */
+	  strcat(name, ".lua");
+	  if(!luaL_loadfile (L, name)) {
+	    if(lua_pcall (L, 0, LUA_MULTRET, 0)) {
+	      report (L);
+	      lua_close (L);
+	      return EXIT_FAILURE;
+	    }
+	  }
+	}
 	size = LoadString(GetModuleHandle(NULL), 1, lua_string,
 			  sizeof(lua_string)/sizeof(TCHAR));
 	if(size) {
-		luaL_openlibs(L);
-		/*_setmode(_fileno(stdin), _O_BINARY);
-		  _setmode(_fileno(stdout), _O_BINARY);*/
-		if (runlua (L, lua_string)) {
-			report (L);
-			lua_close (L);
-			return EXIT_FAILURE;
-		}
+	  if (runlua (L, lua_string)) {
+	    report (L);
+	    lua_close (L);
+	    return EXIT_FAILURE;
+	  }
 	} else {
 	  lua_close(L);
 	  fprintf(stderr, "could not load Lua resource\n");
