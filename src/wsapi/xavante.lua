6e2460815bf05545e4acee4b8c85901c5e2fb318
mascarenhas <mascarenhas>
2009-06-02 16:32:08 +0000

    fixed bug with empty bodies in wsapi.xavante, and added full http status codes to responses

--- src/wsapi/xavante.lua
+++ src/wsapi/xavante.lua
@@ -43,6 +43,49 @@ local function set_cgivars (req, diskpath, path_info_pat, script_name_pat)
    end
 end
 
+local status_codes = {
+   [100] = "Continue",
+   [101] = "Switching Protocols",
+   [200] = "OK",
+   [201] = "Created",
+   [202] = "Accepted",
+   [203] = "Non-Authoritative Information",
+   [204] = "No Content",
+   [205] = "Reset Content",
+   [206] = "Partial Content",
+   [300] = "Multiple Choices",
+   [301] = "Moved Permanently",
+   [302] = "Found",
+   [303] = "See Other",
+   [304] = "Not Modified",
+   [305] = "Use Proxy",
+   [307] = "Temporary Redirect",
+   [400] = "Bad Request",
+   [401] = "Unauthorized",
+   [402] = "Payment Required",
+   [403] = "Forbidden",
+   [404] = "Not Found",
+   [405] = "Method Not Allowed",
+   [406] = "Not Acceptable",
+   [407] = "Proxy Authentication Required",
+   [408] = "Request Time-out",
+   [409] = "Conflict",
+   [410] = "Gone",
+   [411] = "Length Required",
+   [412] = "Precondition Failed",
+   [413] = "Request Entity Too Large",
+   [414] = "Request-URI Too Large",
+   [415] = "Unsupported Media Type",
+   [416] = "Requested range not satisfiable",
+   [417] = "Expectation Failed",
+   [500] = "Internal Server Error",
+   [501] = "Not Implemented",
+   [502] = "Bad Gateway",
+   [503] = "Service Unavailable",
+   [504] = "Gateway Time-out",
+   [505] = "HTTP Version not supported",
+}
+
 local function wsapihandler (req, res, wsapi_run, app_prefix, docroot, app_path)
    local path_info_pat = "^" .. (app_prefix or "") .. "(.*)"
    set_cgivars(req, docroot, path_info_pat, app_prefix)
@@ -57,7 +100,10 @@ local function wsapihandler (req, res, wsapi_run, app_prefix, docroot, app_path)
    wsapi_env.APP_PATH = app_path
 
    local function set_status(status)
-      res.statusline = "HTTP/1.1 " .. tostring(status) 
+     if type(status) == "number" or status:match("^%d+$") then 
+       status = status .. " " .. status_codes[tonumber(status)]
+     end
+     res.statusline = "HTTP/1.1 " .. (status or "500 Internal Server Error")
    end
 
    local function send_headers(headers)
@@ -73,7 +119,8 @@ local function wsapihandler (req, res, wsapi_run, app_prefix, docroot, app_path)
 	 else
             res:add_header(h, tostring(v))
          end
-      end
+       end
+       res:send_headers()
    end
 
    local ok, status, headers, res_iter = common.run_app(wsapi_run, wsapi_env)
