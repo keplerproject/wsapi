7b9be5fdb5642da968cd1c10fe2d01f3be779118
Fabio Mascarenhas <mascarenhas@lambda.local>
2009-10-20 18:37:29 -0200

    teleshake-enabling tests

--- tests/test_request.lua
+++ tests/test_request.lua
@@ -51,7 +51,7 @@ local function is_empty_table(t)
    return true
 end
 
--- Test empty GET
+print("Test empty GET")
 local env = make_env_get()
 local req = wsapi.request.new(env)
 assert(req.path_info == env.PATH_INFO)
@@ -61,35 +61,35 @@ assert(req.query_string == env.QUERY_STRING)
 assert(is_empty_table(req.GET))
 assert(is_empty_table(req.POST))
 
--- Test one-parameter GET
+print("Test one-parameter GET")
 local env = make_env_get("foo=bar")
 local req = wsapi.request.new(env)
 assert(req.GET["foo"] == "bar")
 assert(req.params["foo"] == "bar")
 assert(is_empty_table(req.POST))
 
--- Test one-parameter POST
+print("Test one-parameter POST")
 local env = make_env_post("foo=bar")
 local req = wsapi.request.new(env)
 assert(is_empty_table(req.GET))
 assert(req.POST["foo"] == "bar")
 assert(req.params["foo"] == "bar")
 
--- Test empty POST that is not form-encoded
+print("Test empty POST that is not form-encoded")
 local env = make_env_post(nil, "application/json")
 local req = wsapi.request.new(env)
 assert(is_empty_table(req.GET))
 assert(req.POST["post_data"] == "")
 assert(req.params["post_data"] == "")
 
--- Test POST with content that is not form-encoded
+print("Test POST with content that is not form-encoded")
 local env = make_env_post("{ foo: bar }", "application/json")
 local req = wsapi.request.new(env)
 assert(is_empty_table(req.GET))
 assert(req.POST["post_data"] == "{ foo: bar }")
 assert(req.params["post_data"] == "{ foo: bar }")
 
--- Test two-parameter GET
+print("Test two-parameter GET")
 local env = make_env_get("foo=bar&baz=boo")
 local req = wsapi.request.new(env)
 assert(req.GET["foo"] == "bar")
@@ -98,7 +98,7 @@ assert(req.params["foo"] == "bar")
 assert(req.params["baz"] == "boo")
 assert(is_empty_table(req.POST))
 
--- Test two-parameter POST
+print("Test two-parameter POST")
 local env = make_env_post("foo=bar&baz=boo")
 local req = wsapi.request.new(env)
 assert(is_empty_table(req.GET))
@@ -107,7 +107,7 @@ assert(req.POST["baz"] == "boo")
 assert(req.params["foo"] == "bar")
 assert(req.params["baz"] == "boo")
 
--- Test POST with GET
+print("Test POST with GET")
 local env = make_env_post("baz=boo", nil, "foo=bar")
 local req = wsapi.request.new(env)
 assert(req.GET["foo"] == "bar")
@@ -115,14 +115,14 @@ assert(req.POST["baz"] == "boo")
 assert(req.params["foo"] == "bar")
 assert(req.params["baz"] == "boo")
 
--- Test one-parameter POST
+print("Test one-parameter POST")
 local env = make_env_post("foo=bar")
 local req = wsapi.request.new(env)
 assert(is_empty_table(req.GET))
 assert(req.POST["foo"] == "bar")
 assert(req.params["foo"] == "bar")
 
--- Test multipart/form-data
+print("Test multipart/form-data")
 local boundary = "hello"
 local env = make_env_post(encode_multipart(boundary, { { "foo", "bar\nbar" }, { "baz", "boo" } }),
 					   "multipart/form-data; boundary=" .. boundary)
