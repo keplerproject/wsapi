36d5a762754a1a094bd84017f11a9141438e9ec1
Norman Clarke <norman@njclarke.com>
2010-09-07 10:26:49 -0300

    Don't make each handler define HTTP status codes.

--- src/wsapi/common.lua
+++ src/wsapi/common.lua
@@ -17,6 +17,50 @@ _G.wsapi._COPYRIGHT   = "Copyright (C) 2007-2010 Kepler Project"
 _G.wsapi._DESCRIPTION = "WSAPI - the Lua Web Server API"
 _G.wsapi._VERSION     = "WSAPI 1.3.4"
 
+-- HTTP status codes
+status_codes = {
+   [100] = "Continue",
+   [101] = "Switching Protocols",
+   [200] = "OK",
+   [201] = "Created",
+   [202] = "Accepted",
+   [203] = "Non-Authoritative Information",
+   [204] = "No Content",
+   [205] = "Reset Content",
+   [206] = "Partial Content",
+   [300] = "Multiple Choices",
+   [301] = "Moved Permanently",
+   [302] = "Found",
+   [303] = "See Other",
+   [304] = "Not Modified",
+   [305] = "Use Proxy",
+   [307] = "Temporary Redirect",
+   [400] = "Bad Request",
+   [401] = "Unauthorized",
+   [402] = "Payment Required",
+   [403] = "Forbidden",
+   [404] = "Not Found",
+   [405] = "Method Not Allowed",
+   [406] = "Not Acceptable",
+   [407] = "Proxy Authentication Required",
+   [408] = "Request Time-out",
+   [409] = "Conflict",
+   [410] = "Gone",
+   [411] = "Length Required",
+   [412] = "Precondition Failed",
+   [413] = "Request Entity Too Large",
+   [414] = "Request-URI Too Large",
+   [415] = "Unsupported Media Type",
+   [416] = "Requested range not satisfiable",
+   [417] = "Expectation Failed",
+   [500] = "Internal Server Error",
+   [501] = "Not Implemented",
+   [502] = "Bad Gateway",
+   [503] = "Service Unavailable",
+   [504] = "Gateway Time-out",
+   [505] = "HTTP Version not supported",
+}
+
 -- Makes an index metamethod for the environment, from
 -- a function that returns the value of a server variable
 -- a metamethod lets us do "on-demand" loading of the WSAPI
@@ -97,49 +141,6 @@ function send_content(out, res_iter, write_method)
               tostring(res) .. "\n</pre>")
    end
 end
-
-local status_codes = {
-   [100] = "Continue",
-   [101] = "Switching Protocols",
-   [200] = "OK",
-   [201] = "Created",
-   [202] = "Accepted",
-   [203] = "Non-Authoritative Information",
-   [204] = "No Content",
-   [205] = "Reset Content",
-   [206] = "Partial Content",
-   [300] = "Multiple Choices",
-   [301] = "Moved Permanently",
-   [302] = "Found",
-   [303] = "See Other",
-   [304] = "Not Modified",
-   [305] = "Use Proxy",
-   [307] = "Temporary Redirect",
-   [400] = "Bad Request",
-   [401] = "Unauthorized",
-   [402] = "Payment Required",
-   [403] = "Forbidden",
-   [404] = "Not Found",
-   [405] = "Method Not Allowed",
-   [406] = "Not Acceptable",
-   [407] = "Proxy Authentication Required",
-   [408] = "Request Time-out",
-   [409] = "Conflict",
-   [410] = "Gone",
-   [411] = "Length Required",
-   [412] = "Precondition Failed",
-   [413] = "Request Entity Too Large",
-   [414] = "Request-URI Too Large",
-   [415] = "Unsupported Media Type",
-   [416] = "Requested range not satisfiable",
-   [417] = "Expectation Failed",
-   [500] = "Internal Server Error",
-   [501] = "Not Implemented",
-   [502] = "Bad Gateway",
-   [503] = "Service Unavailable",
-   [504] = "Gateway Time-out",
-   [505] = "HTTP Version not supported",
-}
 
 -- Sends the complete response through the "out" pipe, 
 -- using the provided write method
--- src/wsapi/xavante.lua
+++ src/wsapi/xavante.lua
@@ -46,49 +46,6 @@ local function set_cgivars (req, diskpath, path_info_pat, script_name_pat, extra
   end
 end
 
-local status_codes = {
-  [100] = "Continue",
-  [101] = "Switching Protocols",
-  [200] = "OK",
-  [201] = "Created",
-  [202] = "Accepted",
-  [203] = "Non-Authoritative Information",
-  [204] = "No Content",
-  [205] = "Reset Content",
-  [206] = "Partial Content",
-  [300] = "Multiple Choices",
-  [301] = "Moved Permanently",
-  [302] = "Found",
-  [303] = "See Other",
-  [304] = "Not Modified",
-  [305] = "Use Proxy",
-  [307] = "Temporary Redirect",
-  [400] = "Bad Request",
-  [401] = "Unauthorized",
-  [402] = "Payment Required",
-  [403] = "Forbidden",
-  [404] = "Not Found",
-  [405] = "Method Not Allowed",
-  [406] = "Not Acceptable",
-  [407] = "Proxy Authentication Required",
-  [408] = "Request Time-out",
-  [409] = "Conflict",
-  [410] = "Gone",
-  [411] = "Length Required",
-  [412] = "Precondition Failed",
-  [413] = "Request Entity Too Large",
-  [414] = "Request-URI Too Large",
-  [415] = "Unsupported Media Type",
-  [416] = "Requested range not satisfiable",
-  [417] = "Expectation Failed",
-  [500] = "Internal Server Error",
-  [501] = "Not Implemented",
-  [502] = "Bad Gateway",
-  [503] = "Service Unavailable",
-  [504] = "Gateway Time-out",
-  [505] = "HTTP Version not supported",
-}
-
 local function wsapihandler (req, res, wsapi_run, app_prefix, docroot, app_path, extra_vars)
   local path_info_pat = "^" .. (app_prefix or "") .. "(.*)"
   set_cgivars(req, docroot, path_info_pat, app_prefix, extra_vars)
@@ -104,7 +61,7 @@ local function wsapihandler (req, res, wsapi_run, app_prefix, docroot, app_path,
 
   local function set_status(status)
     if type(status) == "number" or status:match("^%d+$") then 
-      status = status .. " " .. status_codes[tonumber(status)]
+      status = status .. " " .. common.status_codes[tonumber(status)]
     end
     res.statusline = "HTTP/1.1 " .. (status or "500 Internal Server Error")
   end
